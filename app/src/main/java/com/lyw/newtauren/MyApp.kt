0fb700c5e85fd6998bf79d7bcb090a2bfc6d8da9
liyongwei <1255204438@qq.com>
2021-04-13 09:22:54 +0800

    补充

--- app/src/main/java/com/lyw/newtauren/MyApp.kt
+++ app/src/main/java/com/lyw/newtauren/MyApp.kt
@@ -2,6 +2,8 @@ package com.lyw.newtauren
 
 import android.app.Application
 import com.alibaba.android.arouter.launcher.ARouter
+import com.lyw.lyw_common.global.Configurator
+import com.tencent.mmkv.MMKV
 
 /**
  * 功能描述:
@@ -11,6 +13,14 @@ import com.alibaba.android.arouter.launcher.ARouter
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) {
+            ARouter.openDebug()
+        }
+
+        Configurator.instance.withWebApiHost("http://baobab.kaiyanapp.com/api/").configure()
+
+        MMKV.initialize(this)
+        //初始化路由
         ARouter.init(this)
     }
 }
